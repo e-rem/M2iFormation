1017e6bcedf0dbc0416d1afb80d26362780e31e6
rjacquier <rjacquier@horizontalsoftware.com>
2024-05-29 11:22:55 +0200

    exercice 4

--- 04_arrow.js
+++ 04_arrow.js
@@ -7,7 +7,7 @@
  *   - le mot clée "function" est interdis
  */
 
-let arrow1 = () => {}
+let arrow1 = (param1) => { return param1 + 1;};
 
 /**
  * Déclarer une fonction fléchée qui renvoi l'argument + 1
@@ -18,6 +18,6 @@ let arrow1 = () => {}
  *   - le mot clée "return" est interdis
  */
 
-let arrow2 = () => {}
+let arrow2 = (param1) => ( param1 + 1);
 
 module.exports = {arrow1, arrow2};
