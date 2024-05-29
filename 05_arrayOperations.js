5118af68b2d19ad4aa575c42711c44a1a9f3beb9
rjacquier <rjacquier@horizontalsoftware.com>
2024-05-29 12:00:59 +0200

    exercice 5

--- 05_arrayOperations.js
+++ 05_arrayOperations.js
@@ -1,46 +1,57 @@
-
 /**
  * Utiliser la fonction .map sur le tableau passé en paramètre
  * pour retourner un nouveau tableau avec les valeurs multipliées par 2
- * 
- * contraintes: 
+ *
+ * contraintes:
  *   - Les mots clées for, while, do while sont interdits
  *   - les mots clées function et return sont interdits
  *   - Vous ne pouvez pas utiliser de variable
- * 
-  */
+ *
+ */
 
-const multiplyByTwo = (array) => {}
+const array1 = [1, 2, 3, 4];
 
+const multiplyByTwo = (array) => array ? array.map((x) => x * 2) : 0;
+
+console.log(multiplyByTwo(array1));
 
 /**
  * Utiliser la fonction .filter sur le tableau passé en paramètre
  * retourne un nouveau tableau avec uniquement les nom qui commencent par la lettre "A"
- * 
- * contraintes: 
+ *
+ * contraintes:
  *   - Les mots clées for, while, do while sont interdits
  *   - les mots clées function et return sont interdits
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
-  */
+ */
 
-const filterNameStartByA = (array) => {}
+const filterNameStartByA = (array) =>  array.filter((x) => x.startsWith("A"));
 
 /**
  * Utiliser la fonction .reduce sur le tableau passé en paramètre
  * retourne la somme des valeurs du tableau
- * 
- * contraintes: 
+ *
+ * contraintes:
  *   - Les mots clées for, while, do while sont interdits
  *   - les mots clées function et return sont interdits
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
-  */
+ */
 
-const sum = (array) => {}
 
+
+
+const sum = (array) => (
+
+  array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  )
+);
+
+console.log(sum(array1));
 /**
  * Utiliser la fonction .find sur le tableau passé en paramètre
  * retourne l'utilisateur qui a l'id passé en 2e paramètre
- * 
+ *
  * exemple d'entrée:
  * [
  *  {id: 1, name: 'John'},
@@ -49,14 +60,24 @@ const sum = (array) => {}
  *  {id: 4, name: 'Bar'},
  * ], 3
  * retour attendu: "Foo"
- * 
- * contraintes: 
+ *
+ * contraintes:
  *   - Les mots clées for, while, do while sont interdits
  *   - les mots clées function et return sont interdits
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
-  */
+ */
 
-const findUserById = (array, id) => {}
+const array2 = [
+    {id: 1, name: 'John'},
+    {id: 2, name: 'Doe'},
+    {id: 3, name: 'Foo'},
+    {id: 4, name: 'Bar'},
+   ];
 
+const findUserById = (array, id) => (
+  array.find( (x) => x.id === id)?.name ?? null
+);
 
-module.exports = {multiplyByTwo, filterNameStartByA, sum, findUserById};
+console.log(findUserById(array2, 3));
+
+module.exports = { multiplyByTwo, filterNameStartByA, sum, findUserById };
